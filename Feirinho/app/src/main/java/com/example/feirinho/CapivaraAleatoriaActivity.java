984d9dd5120a412148eca2d951ba1e94b3ac403b
hendrik S2 <hendrik2002.hkk@gmail.com>
2023-05-29 17:52:33 -0300

    quase prontogit pullgit pull

--- Feirinho/app/src/main/java/com/example/feirinho/CapivaraAleatoriaActivity.java
+++ Feirinho/app/src/main/java/com/example/feirinho/CapivaraAleatoriaActivity.java
@@ -3,6 +3,7 @@ package com.example.feirinho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import java.text.Normalizer;
 import java.util.ArrayList;
@@ -22,8 +24,11 @@ public class CapivaraAleatoriaActivity extends AppCompatActivity {
 
     TextView txtNomeCurso;
     ImageButton btnVoltar;
-    ImageView imgCapivara;
+    ImageView imgCapivara, imgPapelDeParede;
+    VideoView videoPapelDeParede;
     Handler handler;
+
+    int i = 0;
 
     List<String> listaCursos = new ArrayList<>();
 
@@ -34,9 +39,14 @@ public class CapivaraAleatoriaActivity extends AppCompatActivity {
         imgCapivara = findViewById(R.id.imgCapivara);
         txtNomeCurso = findViewById(R.id.txtNomeCurso);
         btnVoltar = findViewById(R.id.btnVoltar);
+   //     videoPapelDeParede = findViewById(R.id.videoPapelDeParede);
+        imgPapelDeParede = findViewById(R.id.imgPapelDeParede);
+
         handler = new Handler();
 
         inserirCursos();
+        //inserirPapelDeParede();
+        checarImagensPorCurso();
 
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,10 +57,25 @@ public class CapivaraAleatoriaActivity extends AppCompatActivity {
 
         onViewCreated();
     }
+   /* private void inserirPapelDeParede() {
+        String videoPath;
+
+        if (i % 2 == 0) {
+            videoPath = "android.resource://" + getPackageName() + "/" + R.raw.fundo_biologicas; // Substitua "video_file" pelo nome do seu vídeo na pasta "res/raw"
+        } else {
+            videoPath = "android.resource://" + getPackageName() + "/" + R.raw.fundo_tecnologia; // Substitua "video_file" pelo nome do seu vídeo na pasta "res/raw"
+        }
+        Uri uri = Uri.parse(videoPath);
+        videoPapelDeParede.setVideoURI(uri);
+        videoPapelDeParede.start();
+    }
+    */
+
 
     public void onViewCreated() {
         //inicia uma thread, para que renderize a view, mas possa aleatorizar o curso
         new Thread(this::aleatorizarCurso).start();
+
     }
 
     @Override
@@ -89,6 +114,7 @@ public class CapivaraAleatoriaActivity extends AppCompatActivity {
                 public void run() {
                     //só podemos mexer nos componentes da tela na thread principal
                     txtNomeCurso.setText(curso);
+                    inserirPapelDeParede(curso);
                     String arquivo = converterCursoParaNomeArquivo(curso);
                     int idArquivo = getResources().getIdentifier(arquivo, "drawable", getPackageName());
 
@@ -110,6 +136,127 @@ public class CapivaraAleatoriaActivity extends AppCompatActivity {
             ultimoCurso = proximoCurso;
         }
 
+    }
+
+    void inserirPapelDeParede(String curso) {
+        // TODO refatorar para criar uma classe curso que terá um atributo setor
+        // ou ainda
+
+        if (curso.equalsIgnoreCase("Agronomia") || curso.equalsIgnoreCase("Engenharia Florestal")
+                || curso.equalsIgnoreCase("Engenharia Industrial Madeireira") || curso.equalsIgnoreCase("Zootecnia")) {
+            // Setor de Ciências Agrárias
+            imgPapelDeParede.setImageResource(R.drawable.fundo_agrarias);
+
+        } else if (curso.equalsIgnoreCase("Biomedicina") || curso.equalsIgnoreCase("Ciências Biológicas")
+                || curso.equalsIgnoreCase("Educação Física") || curso.equalsIgnoreCase("Fisioterapia")) {
+            // Setor de Ciências Biológicas
+            imgPapelDeParede.setImageResource(R.drawable.fundo_biologicas);
+
+        } else if (curso.equalsIgnoreCase("Ciência da Computação") || curso.equalsIgnoreCase("Análise e Desenvolvimento de Sistemas")
+                || curso.equalsIgnoreCase("Estatística") || curso.equalsIgnoreCase("Expressão Gráfica")
+                || curso.equalsIgnoreCase("Física") || curso.equalsIgnoreCase("Informática Biomédica")
+                || curso.equalsIgnoreCase("Matemática") || curso.equalsIgnoreCase("Matemática Industrial")
+                || curso.equalsIgnoreCase("Química")) {
+            // Setor de Ciências Exatas
+            imgPapelDeParede.setImageResource(R.drawable.fundo_exatas);
+
+        } else if (curso.equalsIgnoreCase("Direito")) {
+            // Setor de Ciências Jurídicas
+            imgPapelDeParede.setImageResource(R.drawable.fundo_juridicas);
+
+        } else if (curso.equalsIgnoreCase("Enfermagem") || curso.equalsIgnoreCase("Farmácia")
+                || curso.equalsIgnoreCase("Medicina") || curso.equalsIgnoreCase("Nutrição")
+                || curso.equalsIgnoreCase("Odontologia") || curso.equalsIgnoreCase("Terapia Ocupacional")) {
+            // Setor de Ciências da Saúde
+            imgPapelDeParede.setImageResource(R.drawable.fundo_saude);
+
+        } else if (curso.equalsIgnoreCase("Administração") || curso.equalsIgnoreCase("Ciências Contábeis")
+                || curso.equalsIgnoreCase("Ciências Econômicas") || curso.equalsIgnoreCase("Ciência e Gestão da Informação")) {
+            // Setor de Ciências Sociais Aplicadas
+            imgPapelDeParede.setImageResource(R.drawable.fundo_ciencias_sociais);
+
+        } else if (curso.equalsIgnoreCase("Geografia") || curso.equalsIgnoreCase("Geologia")
+                || curso.equalsIgnoreCase("Geomática (Engenharia Cartográfica)")) {
+            // Setor de Ciências da Terra
+            imgPapelDeParede.setImageResource(R.drawable.fundo_ciencias_da_terra);
+
+        } else if (curso.equalsIgnoreCase("Pedagogia")) {
+            // Setor de Educação
+            imgPapelDeParede.setImageResource(R.drawable.fundo_educacao);
+
+        } else if (curso.equalsIgnoreCase("Arquitetura e Urbanismo") || curso.equalsIgnoreCase("Engenharia Civil")
+                || curso.equalsIgnoreCase("Engenharia Elétrica") || curso.equalsIgnoreCase("Engenharia Mecânica")
+                || curso.equalsIgnoreCase("Engenharia Ambiental") || curso.equalsIgnoreCase("Engenharia de Produção")
+                || curso.equalsIgnoreCase("Engenharia de Aquicultura") || curso.equalsIgnoreCase("Engenharia de Bioprocessos e Biotecnologia")
+                || curso.equalsIgnoreCase("Engenharia de Alimentos") || curso.equalsIgnoreCase("Engenharia Química")) {
+            // Setor de Tecnologia
+            imgPapelDeParede.setImageResource(R.drawable.fundo_tecnologia);
+
+        } else if (curso.equalsIgnoreCase("Administração Pública") || curso.equalsIgnoreCase("Agroecologia")
+                || curso.equalsIgnoreCase("Artes") || curso.equalsIgnoreCase("Ciências")
+                || curso.equalsIgnoreCase("Ciências Ambientais") || curso.equalsIgnoreCase("Educação do Campo")
+                || curso.equalsIgnoreCase("Educação Física") || curso.equalsIgnoreCase("Geografia")
+                || curso.equalsIgnoreCase("Gestão de Turismo") || curso.equalsIgnoreCase("Gestão e Empreendedorismo")
+                || curso.equalsIgnoreCase("Gestão Imobiliária") || curso.equalsIgnoreCase("Linguagem e Comunicação")
+                || curso.equalsIgnoreCase("Saúde Coletiva") || curso.equalsIgnoreCase("Serviço Social")
+                || curso.equalsIgnoreCase("Artes Visuais")) {
+            // Campus UFPR Litoral
+            imgPapelDeParede.setImageResource(R.drawable.fundo_litoral);
+
+        } else if (curso.equalsIgnoreCase("Oceanografia") || curso.equalsIgnoreCase("Ciências Exatas")
+                || curso.equalsIgnoreCase("Engenharia Civil") || curso.equalsIgnoreCase("Engenharia de Aquicultura")
+                || curso.equalsIgnoreCase("Engenharia Ambiental e Sanitária")) {
+            // Campus Centro de Estudos do Mar
+            imgPapelDeParede.setImageResource(R.drawable.fundo_estudos_do_mar);
+
+        } else if (curso.equalsIgnoreCase("Agronomia") || curso.equalsIgnoreCase("Ciências Biológicas")
+                || curso.equalsIgnoreCase("Engenharia de Aquicultura") || curso.equalsIgnoreCase("Engenharia de Bioprocessos e Biotecnologia")
+                || curso.equalsIgnoreCase("Engenharia de Energia") || curso.equalsIgnoreCase("Licenciatura em Ciências Exatas")
+                || curso.equalsIgnoreCase("Licenciatura em Computação") || curso.equalsIgnoreCase("Medicina Veterinária")
+                || curso.equalsIgnoreCase("Computação (Licenciatura)")) {
+            // Campus Palotina
+            imgPapelDeParede.setImageResource(R.drawable.fundo_palotina);
+
+        } else if (curso.equalsIgnoreCase("Análise e Desenvolvimento de Sistemas") || curso.equalsIgnoreCase("Comunicação Institucional")
+                || curso.equalsIgnoreCase("Gestão de Qualidade") || curso.equalsIgnoreCase("Gestão Pública")
+                || curso.equalsIgnoreCase("Luteria") || curso.equalsIgnoreCase("Negócios Imobiliários")
+                || curso.equalsIgnoreCase("Produção Cênica") || curso.equalsIgnoreCase("Secretariado")
+                || curso.equalsIgnoreCase("Petróleo e Gás") || curso.equalsIgnoreCase("Agente Comunitário de Saúde")) {
+            // SEPT
+            imgPapelDeParede.setImageResource(R.drawable.fundo_sept);
+        } else if (curso.equalsIgnoreCase("Ciências Sociais") || curso.equalsIgnoreCase("História")
+                || curso.equalsIgnoreCase("Filosofia") || curso.equalsIgnoreCase("Letras")
+                || curso.equalsIgnoreCase("Letras Libras") || curso.equalsIgnoreCase("Psicologia")
+                || curso.equalsIgnoreCase("Turismo")) {
+            // Setor de Ciências Humanas, Letras e Artes
+            imgPapelDeParede.setImageResource(R.drawable.fundo_ciencias_humanas);
+
+        } else {
+            Log.d("teste","O curso "+curso+" não foi encontrado");
+        }
+    }
+
+
+    private void checarImagensPorCurso() {
+        int qtdade_nomes_errados = 0;
+        for (int i = 0; i < QUANTIDADE_CURSOS_EXIBIR; i++) {
+            String curso = listaCursos.get(i);
+            String arquivo = converterCursoParaNomeArquivo(curso);
+            int idArquivo = getResources().getIdentifier(arquivo, "drawable", getPackageName());
+
+            if (idArquivo == 0) {
+                Log.d("teste", "O arquivo " + arquivo + " não existe");
+                qtdade_nomes_errados++;
+            }
+
+            //inserirPapelDeParede(curso);
+        }
+
+        if (qtdade_nomes_errados == 0) {
+            Log.d("teste", "Todos os cursos da lista tem arquivos e seus nomes estão corretos.");
+        } else {
+            Log.d("teste", qtdade_nomes_errados + " arquivos com nomes errados!");
+        }
     }
 
 
@@ -207,8 +354,8 @@ public class CapivaraAleatoriaActivity extends AppCompatActivity {
     private String converterCursoParaNomeArquivo(String curso) {
         curso = curso.toLowerCase();
         curso = Normalizer.normalize(curso, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
-        curso = curso.replaceAll(" ", "_");
-        curso = curso.replaceAll(",", "");  //História, memória e imagem ==> historia_memoria_e_imagem
+        curso = curso.replaceAll(" ", "_");  //troca espaço por _
+        curso = curso.replaceAll("[,)(]", "");  //remove vírgulas e parêntesis
         return curso;
     }
 }
